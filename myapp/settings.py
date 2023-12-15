fc3f9fb455b34bee9140fa158661028c723c0a95
Okemwag <gabrielokemwa83@gmail.com>
2023-12-15 16:11:40 +0300

    Update database configuration in settings.py

--- myapp/settings.py
+++ myapp/settings.py
@@ -109,14 +109,14 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 DATABASES = {
     "default": {
